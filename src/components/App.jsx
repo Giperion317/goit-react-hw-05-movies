679dc05570642cfe11093a3b76bb68cd4d29a56f
Anatolii Pismennyi <105397905+Giperion317@users.noreply.github.com>
2022-12-26 04:44:37 +0100

    Update lazy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,41 @@
-import { lazy } from "react";
-import { Routes, Route } from "react-router-dom";
-import { GlobalStyle } from "utils/GlobalStyles";
-import { Layout } from "./Layout/Layout";
+import { lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { GlobalStyle } from 'utils/GlobalStyles';
+import { Layout } from './Layout/Layout';
 
-const HomePage = lazy(() => import('pages/HomePage'))
-const MoviesPage = lazy(() => import('pages/MoviesPage'))
-const MovieFullInfoPage = lazy(() => import('pages/MovieFullInfoPage'))
-const Cast = lazy(() => import('pages/Cast/Cast'))
-const Reviews = lazy(() => import('pages/Reviews/Reviews'))
+const HomePage = lazy(() =>
+  import('pages/HomePage' /* webpackChunkName: "home-page" */)
+);
+const MoviesPage = lazy(() =>
+  import('pages/MoviesPage' /* webpackChunkName: "movies-page" */)
+);
+const MovieFullInfoPage = lazy(() =>
+  import(
+    'pages/MovieFullInfoPage' /* webpackChunkName: "movie-full-info-page" */
+  )
+);
+const Cast = lazy(() =>
+  import('pages/Cast/Cast' /* webpackChunkName: "cast-subpage" */)
+);
+const Reviews = lazy(() =>
+  import('pages/Reviews/Reviews' /* webpackChunkName: "reviews-subpage" */)
+);
 
 export const App = () => {
-  
   return (
     <>
       <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route index element={<HomePage/>} />
-          <Route path='movies' element={<MoviesPage />} />
-          <Route path='movies/:movieId' element={<MovieFullInfoPage />}>
-            <Route path='cast' element={<Cast/>} />
-            <Route path='reviews' element={<Reviews/>} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="movies" element={<MoviesPage />} />
+          <Route path="movies/:movieId" element={<MovieFullInfoPage />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
           </Route>
-            <Route path="*" element={<HomePage />} />
+          <Route path="*" element={<HomePage />} />
         </Route>
-        </Routes>
+      </Routes>
       <GlobalStyle />
-      </>
+    </>
   );
 };
